c9acc9f009bbb146530c516dc8e2429cf7822f56
Aiden Somboon <aidensomboon07@gmail.com>
2022-07-31 18:55:45 +0800

    V0.0.2

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -22,7 +22,7 @@ function Index() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/pages/Index.jsx</code> and save to edit this page.
         </p>
       </div>
       <p className="read-the-docs">
